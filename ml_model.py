44259e932cdf83b42e1abe55eed9a5589a467175
Nekhita Sri <mnekhitasri@gmail.com>
2026-02-09 17:17:02 +0530

    Added ML module files

---
+++ ml_model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("final-v1.csv")
+
+# Features and target
+X = df.drop('is_fake', axis=1)
+y = df['is_fake']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
+import joblib
+
+joblib.dump(model, "fake_detector.pkl")
+print("Model saved successfully")
